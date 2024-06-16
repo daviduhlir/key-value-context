a5c572bc305787e6ca5252622cb4af5c495dfe7f
DavidUhlíř <posta@daviduhlir.cz>
2024-06-16 15:13:53 +0200

    preapred for first release

--- dist/Context.js
+++ dist/Context.js
@@ -1,7 +1,19 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Context = exports.CONTEXT_BASE_CONFIG = void 0;
-const AsyncLocalStorage_1 = require("./utils/AsyncLocalStorage");
+const AsyncLocalStorage_1 = __importDefault(require("./utils/AsyncLocalStorage"));
 exports.CONTEXT_BASE_CONFIG = {
     flushIfFail: false,
     deleteUndefined: true,
@@ -10,10 +22,7 @@ class Context {
     constructor(topData = {}, config = {}) {
         this.topData = topData;
         this.stackStorage = new AsyncLocalStorage_1.default();
-        this.config = {
-            ...exports.CONTEXT_BASE_CONFIG,
-            ...config,
-        };
+        this.config = Object.assign(Object.assign({}, exports.CONTEXT_BASE_CONFIG), config);
     }
     getValue(key) {
         const stack = [this.topData, ...(this.stackStorage.getStore() || [])];
@@ -29,38 +38,39 @@ class Context {
         const topItem = stack[stack.length - 1];
         topItem[key] = value;
     }
-    async runInContext(handler) {
-        const stack = [this.topData, ...(this.stackStorage.getStore() || [])];
-        const actualData = {};
-        let result;
-        let error;
-        try {
-            result = await this.stackStorage.run([...stack, actualData], async () => handler());
-        }
-        catch (e) {
-            error = e;
-        }
-        if ((error && this.config.flushIfFail) || !error) {
-            const topItem = stack[stack.length - 1];
-            const keys = Object.keys(actualData);
-            for (const key of keys) {
-                ;
-                topItem[key] = actualData[key];
+    runInContext(handler) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const stack = [this.topData, ...(this.stackStorage.getStore() || [])];
+            const actualData = {};
+            let result;
+            let error;
+            try {
+                result = yield this.stackStorage.run([...stack, actualData], () => __awaiter(this, void 0, void 0, function* () { return handler(); }));
             }
-        }
-        if (this.config.deleteUndefined && stack.length === 1) {
-            const keys = Object.keys(stack[0]);
-            for (const key of keys) {
-                if (typeof stack[0][key] === 'undefined') {
-                    delete stack[0][key];
+            catch (e) {
+                error = e;
+            }
+            if ((error && this.config.flushIfFail) || !error) {
+                const topItem = stack[stack.length - 1];
+                const keys = Object.keys(actualData);
+                for (const key of keys) {
+                    ;
+                    topItem[key] = actualData[key];
                 }
             }
-        }
-        if (error) {
-            throw error;
-        }
-        return result;
+            if (this.config.deleteUndefined && stack.length === 1) {
+                const keys = Object.keys(stack[0]);
+                for (const key of keys) {
+                    if (typeof stack[0][key] === 'undefined') {
+                        delete stack[0][key];
+                    }
+                }
+            }
+            if (error) {
+                throw error;
+            }
+            return result;
+        });
     }
 }
 exports.Context = Context;
-//# sourceMappingURL=Context.js.map
