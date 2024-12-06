7687c6904e6003dab06a1067bf7487656f2fb193
david.uhlir <posta@daviduhlir.cz>
2024-12-06 08:15:29 +0100

    deleted dist

--- dist/utils/AsyncLocalStorage.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class AsyncLocalStorageMock<T = any> {
-    constructor();
-    getStore(): T;
-    run(data: T, fnc: any): Promise<any>;
-}
-declare let AsyncLocalStorageClass: typeof AsyncLocalStorageMock;
-export default AsyncLocalStorageClass;
